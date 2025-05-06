ec06532b8bf02e20ab05dac79b01035c762ef84b
rvarletabio <rvarleta@bioextrusion.com>
2025-05-06 11:59:49 -0400

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,4 @@
-/* --------- CONFIG: elige endpoint local vs. Render --------- */
-const API =
-  location.hostname === "127.0.0.1" || location.hostname === "localhost"
-    ? "http://127.0.0.1:5000/predict"                 // cuando trabajas en tu PC
-    : "https://cyllium-backend.onrender.com/predict"; // cuando la web está online
-/* ----------------------------------------------------------- */
+/* ---------- CONFIG ---------- */
+const API = "https://cyllium-backend.onrender.com/";   // ← cambia esto
+/* ---------------------------- */
+
